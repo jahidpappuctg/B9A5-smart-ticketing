adf4f6495caeccd1cf8c5bb0659d71aaefdb37e6
jahidpappuctg <jahidpappublog@gmail.com>
2024-02-18 15:36:43 +0600

    updated

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log("conneted");
+        const seats = document.querySelectorAll('.seat');
+        const totalPriceDisplay = document.getElementById('total-price');
+        
+        const couponCodeInput = document.getElementById('coupon-code');
+        
+        const applyCouponButton = document.getElementById('apply-coupon');
+        const nextPageButton = document.getElementById('next-page');
+
